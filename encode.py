22a18b2a283f120c879515863f6ffdc472fd903d
Aditya Saky <aditya.sirish@gmail.com>
2015-06-21 18:51:26 +0530

    Formatting

--- encode.py
+++ encode.py
@@ -3,9 +3,9 @@ print "\nThis is a simple md5sum encoder I wrote in Python.";
 print "\nYou can find this on http://github.com/paradoxdjell/hashcracker"
 
 inp = raw_input("\nEnter the word whose md5sum is to be found: ")
-print "\nYou entered ", inp
+print "\nYou entered ",inp
 
 import hashlib
 
 s = hashlib.md5(inp.encode())
-print "\nThe md5sum is ", s.hexdigest()
+print "\nThe md5sum is ",s.hexdigest(),"."
