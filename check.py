8b5d495ad38ec05703d565bb3f664c260f949227
Aditya Saky <aditya.sirish@gmail.com>
2015-06-21 18:49:50 +0530

    Remove pesky class

--- check.py
+++ check.py
@@ -4,13 +4,7 @@ print "\nYou can find this on http://github.com/paradoxdjell/hashcracker"
 print "\nFeel free to use your own wordlist. Just replace words.txt with your own file and ensure it is named words.txt.";
 
 inp = raw_input("\nEnter the md5 sum: ")
-print "\nYou entered ", inp
-
-class A:
-    def assignment(self):
-        line = fh.readline()
-
-a = A()
+print "\nYou entered ",inp
 
 import hashlib
 
@@ -19,14 +13,13 @@ fh = open("words.txt", "r")
 flag = 0;
 
 for line in fh:
-    a.assignment
-    s = hashlib.md5(line.encode())
+    s = hashlib.md5(line.rstrip().encode())
     if inp == s.hexdigest():
-        print "\nMatch found! - ", line
+        print "\nMatch found! - ",line
         flag = 1;
         break
 
 if flag == 0:
-    print "\nNo match found."
+    print "\nNo match found. Try a different wordlist?"
 
 fh.close();
